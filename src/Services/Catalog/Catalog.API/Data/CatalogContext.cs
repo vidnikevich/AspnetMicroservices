97422e6f815a65649712f5be4df9d91b11bc5173
vadim.vidnikevich <vadim.vidnikevich@users.noreply.github.com>
2023-07-15 14:22:20 +0200

    catalog.api

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,18 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICantalogContext
+    {
+        public CatalogContext(IConfiguration configuration) 
+        { 
+            var client = new MongoClient(configuration.GetSection("DatabaseSettings").GetValue<string>("ConnectionString"));
+            var database = client.GetDatabase(configuration.GetSection("DatabaseSettings").GetValue<string>("DatabaseName"));
+            Products = database.GetCollection<Product>(configuration.GetSection("DatabaseSettings").GetValue<string>("ConnectionName"));
+            CatalogContextSeed.SeedData(Products);
+        }
+
+        public IMongoCollection<Product> Products { get; }
+    }
+}
