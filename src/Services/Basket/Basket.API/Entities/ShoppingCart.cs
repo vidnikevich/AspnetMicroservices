78bb74cfddd42ac2331721c4cce09126cc705e2d
vadim.vidnikevich <vadim.vidnikevich@users.noreply.github.com>
2023-07-15 15:27:23 +0200

    basket api

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,19 @@
+﻿namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+        public string UserName { get; set; } = string.Empty;
+        public List<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
+
+        public decimal TotalPrice => Items.Sum(i => i.Price);
+        public ShoppingCart()
+        {
+            
+        }
+
+        public ShoppingCart(string userName)
+        {
+            UserName = userName;
+        }
+    }
+}
